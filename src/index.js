4019c884100c1276f65abd55e41f338e20ab8ebe
connorAlex <alex.connor.cochrane@gmail.com>
2022-07-22 13:03:21 -0500

    added js DOM creation => initialLoad()

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ import './style.css';
 const page = document.querySelector('.content');
 
 // load core page elements
-page.appendChild(initialLoad());
+let elements = initialLoad();
+for (const [key, value] of Object.entries(elements)){
+    page.appendChild(value);
+}
+
+// elements.forEach((e) => {
+//     page.appendChild(e);
+// });
--- src/pageLoad.js
+++ src/pageLoad.js
@@ -6,12 +6,34 @@ const initialLoad = () => {
     nav.innerHTML = "Restaurant Name";
 
     // create container
+    const container = document.createElement('div');
+    container.classList.add('container');
 
     // create pages
+    const pages = document.createElement('div');
+    pages.classList.add('pages');
+
+    container.appendChild(pages);
+
+    // create page buttons
+    const about = document.createElement('button');
+    about.innerHTML = "About";
+
+    const food = document.createElement('button');
+    food.innerHTML = "Food";
+
+    const drinks = document.createElement('button');
+    drinks.innerHTML = "Food";
+
+    pages.appendChild(about);
+    pages.appendChild(food);
+    pages.appendChild(drinks);
 
     // create footer
+    const footer = document.createElement('footer');
+    footer.innerHTML = "Footer";
 
-    return nav;
+    return {nav , container, footer};
 }
 
 export {initialLoad};
