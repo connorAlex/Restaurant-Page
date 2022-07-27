82054a3bca50e389bd8be36c463583b45d1b1e3b
connorAlex <alex.connor.cochrane@gmail.com>
2022-07-27 13:10:10 -0500

    refactored menu creation

--- src/menu.js
+++ src/menu.js
@@ -1,25 +1,51 @@
 const menuLoad = () => {
 
+    const menu = {
+        apps: [
+            ['Brandade de Morue', 'BDM Description'],
+            ['French Onion Soup', 'Soup Description']
+        ],
+
+        entrees: [
+            ['Coq au Vin', 'CAV description'],
+            ['Entree 2', 'Entree2 Description']
+        ],
+
+        desserts: [
+            ['Charlotte de Marons', 'CDM Placeholder'],
+            ['Creme Brulee', 'Creme Brulee Placeholder']
+        ]
+    };
+
     // create the new subject element
     const item = document.createElement('div');
     item.classList.add('item');
 
 
-    // create card
-    const card = document.createElement('div');
-    card.classList.add('card');
+    function createCard(course) {
+        course.forEach((e) => {
+            //create a card
+            const card = document.createElement('div');
+            card.classList.add('card');
 
-    // create the title + description
-    const title = document.createElement('div');
-    title.innerHTML = "Title of Food";
+            //create a title div
+            const title = document.createElement('div');
+            title.innerHTML = e[0];
 
-    const desc = document.createElement('div');
-    desc.innerHTML = "This is a description of food";
+            //create the description div
+            const desc = document.createElement('div');
+            desc.innerHTML = e[1];
 
-    card.appendChild(title);
-    card.appendChild(desc);
+            card.appendChild(title);
+            card.appendChild(desc);
+            
+            item.appendChild(card);
+        });
+    };
 
-    item.appendChild(card);
+    createCard(menu.apps);
+    createCard(menu.entrees);
+    createCard(menu.desserts);
 
     return item;
 }
